3c90db794b523f4d92fd1e07803aa26eda6cafe2
De Silva, Dinuka <l.dinukadesilva@gmail.com>
2017-08-23 14:52:10 +0530

    gh-5: Added the initial test setup with mocha and istanbul
    This fixes #5

---
+++ test/spec.js
@@ -0,0 +1,10 @@
+var gulpDependencyInstall = require("../index");
+
+describe("gulp-dependancy-install", function () {
+
+  //TODO add test cases
+
+  it("should be injectable as a common js module", function () {
+    gulpDependencyInstall;
+  });
+});
