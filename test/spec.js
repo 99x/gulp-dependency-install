9518b3e2548febae846ca69914d0f64572cfce83
De Silva, Dinuka <l.dinukadesilva@gmail.com>
2017-08-23 16:05:47 +0530

    Refactoring: Updated all the indexes and line spaces

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,6 @@
 var gulpDependencyInstall = require("../index");
 
 describe("gulp-dependancy-install", function () {
-
   //TODO add test cases
 
   it("should be injectable as a common js module", function () {
